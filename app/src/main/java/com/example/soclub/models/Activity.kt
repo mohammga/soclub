35b5e12f653fd4db9d095c5d83186ba1da7a443a
abodixz <122395857+abodixz@users.noreply.github.com>
2024-10-15 17:52:42 +0200

    update

--- app/src/main/java/com/example/soclub/models/Activity.kt
+++ app/src/main/java/com/example/soclub/models/Activity.kt
@@ -9,11 +9,14 @@ data class Activity(
     val imageUrl: String = "",
     val title: String = "",
     val description: String = "",
-    val ageGroup: Int = 0 ,
+    val ageGroup: Int = 0,
     val maxParticipants: Int = 0,
     val location: String = "",
     val restOfAddress: String = "",
-    val date: String = ""
+    val date: Timestamp? = null,
+    val creatorId: String = "",
+    val createdAt: Timestamp = Timestamp.now(),
+    val startTime: String = ""
 )
 
 
