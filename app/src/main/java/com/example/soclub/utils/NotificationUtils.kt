b0523f06ac95a06eaf973b095f7edff2fed318e2
Gorgostammos <90147575+Gorgostammos@users.noreply.github.com>
2024-11-20 16:56:35 +0100

    strings

--- app/src/main/java/com/example/soclub/utils/NotificationUtils.kt
+++ app/src/main/java/com/example/soclub/utils/NotificationUtils.kt
@@ -3,13 +3,16 @@ package com.example.soclub.utils
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-
+import com.example.soclub.R
 
 
 fun createNotificationChannel(context: Context) {
-    val channelId = "activity_reminder_channel"
-    val channelName = "Activity Reminder Notifications"
-    val channelDescription = "Notifications for activity reminders and updates"
+    //val channelId = "activity_reminder_channel"
+    //val channelName = "Activity Reminder Notifications"
+    //val channelDescription = "Notifications for activity reminders and updates"
+    val channelId = context.getString(R.string.channel_id)
+    val channelName = context.getString(R.string.channel_name)
+    val channelDescription = context.getString(R.string.channel_description)
     val importance = NotificationManager.IMPORTANCE_HIGH
     val channel = NotificationChannel(channelId, channelName, importance).apply {
         description = channelDescription
